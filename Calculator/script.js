7a571376ac9a781ca85da3ead38bb085d07a1b23
Pratyush <chitrakarpratyush@gmail.com>
2025-01-18 18:57:24 +0545

    calculator1.2

---
+++ Calculator/script.js
@@ -0,0 +1,62 @@
+let buttons = document.querySelectorAll("button");
+let screen = document.querySelector("input");
+let string = "";
+let numVal = "";
+let bracket = "(";
+
+let toggleForBracket = () => {
+  if (bracket === "(") {
+    key = "(";
+    string += key;
+    screen.value = string;
+    bracket = ")";
+  } else {
+    key = ")";
+    string += key;
+    screen.value = string;
+    bracket = "(";
+  }
+};
+
+let toggleForIncDec = () => {
+  let currentValue=parseFloat(screen.value);
+  currentValue= -currentValue;
+  screen.value=currentValue;
+  string= currentValue.toString();
+};
+
+let flip = (key) => {
+  if (key === "()") {
+    toggleForBracket();
+  } else {
+    toggleForIncDec();
+  }
+};
+
+let calculation = (string) => {
+  let result = eval(string);
+  return result;
+};
+
+let keyValSeperation = (evt) => {
+  key = evt.target.dataset.key;
+  if (key === "C") {
+    string = "";
+    bracket = "(";
+    incDec = "+";
+    screen.value = string;
+  } else if (key === "=") {
+    let result = calculation(string);
+    screen.value = result;
+    string=result.toString();
+  } else if (key === "()") {
+    flip(key);
+  } else if (key === "#") {
+    flip(key);
+  } else {
+    string += key;
+    screen.value = string;
+  }
+};
+
+buttons.forEach((button) => button.addEventListener("click", keyValSeperation));
